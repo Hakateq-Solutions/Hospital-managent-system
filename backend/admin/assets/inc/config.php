a4ebc0ed890e049c19487f910fdc7795fe3ce591
kyomuhendo-isihaka <kyomuhendoisihaka1@gmail.com>
2025-02-05 19:26:10 +0300

    first commit

---
+++ backend/admin/assets/inc/config.php
@@ -0,0 +1,11 @@
+<?php
+$dbuser="hakabvzf_isihaka";
+$dbpass="Isihaka1005";
+$host="localhost";
+$db="hakabvzf_hospital";
+// $dbuser="root";
+// $dbpass="";
+// $host="localhost";
+// $db="hmisphp";
+$mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+?>
