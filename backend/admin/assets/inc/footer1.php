4d95ad845755b5681fb9485774512a6eaf557ebc
kyomuhendo-isihaka <kyomuhendoisihaka1@gmail.com>
2025-02-06 04:05:47 +0300

    updated

--- backend/admin/assets/inc/footer1.php
+++ backend/admin/assets/inc/footer1.php
@@ -1,3 +1,4 @@
 <footer class="footer footer-alt">
-    2020 - <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By <a href="hakateq.com">Hakateq Solutions</a>
+   <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By <a href="https://hakateq.com/">Hakateq Solutions</a>
 </footer>
+
--- backend/doc/assets/inc/footer.php
+++ backend/doc/assets/inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                2020 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By Martin Mbithi Nzilani</a>
+                <?php echo date('Y'); ?> &copy; Hospital Management System. Developed By <a href="https://hakateq.com/">Hakateq Solutions</a>
             </div>
 
         </div>
